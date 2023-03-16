e16bc90a26dc24e8abf69e9038667bcf642da8e5
Sonicdaheghod <68253811+Sonicdaheghod@users.noreply.github.com>
2023-03-16 12:22:32 -0400

    Add files via upload

---
+++ Question4.AreaCircle.py
@@ -0,0 +1,13 @@
+#4- Write a Python program that calculates the area of a circle based on the radius entered by the user.
+# The purpose of this activity is to practice on implementing math functions to have a desired output for real life applications.
+
+
+#givens: r = 1.1
+#formula = pi(r^2)
+
+#1-parameters
+radius = 1.1
+
+Area = int(3.14*radius)
+#2- output area
+print("The area of a bubble with a radius of 1.1 meters is", Area , "meters")
