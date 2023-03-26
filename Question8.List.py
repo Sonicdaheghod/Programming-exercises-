792555b4c6afb86b323d0d5e56cb8a62cb1f8646
Sonicdaheghod <68253811+Sonicdaheghod@users.noreply.github.com>
2023-03-25 20:18:00 -0400

    Add files via upload

---
+++ Question8.List.py
@@ -0,0 +1,8 @@
+#8. Write a Python program to display the first and last colors from the following list. Go to the editor
+#color_list = ["Red","Green","White" ,"Black"]
+
+listOfSonic = ["Sonic", "Tails", "Knuckles"]
+firstCharacter = listOfSonic[0]
+lastCharacter = listOfSonic[2]
+
+print("The first and last characters in the list", listOfSonic, "are", firstCharacter,"and", lastCharacter)
