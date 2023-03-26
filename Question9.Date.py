ada069c84d70247f0297c48d844006efd690117e
Sonicdaheghod <68253811+Sonicdaheghod@users.noreply.github.com>
2023-03-26 17:56:49 -0400

    Add files via upload
    in progress

---
+++ Question9.Date.py
@@ -0,0 +1,7 @@
+#Write a Python program to display the examination schedule. (extract the date from exam_st_date). Go to the editor
+#exam_st_date = (11, 12, 2014)
+#Sample Output : The examination will start from : 11 / 12 / 2014
+
+from datetime import date
+examDate = (2/15/2023)
+examText = ("The examination will start from: ", date(examDate)
