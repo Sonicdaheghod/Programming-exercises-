95ed4ea80b784eaedd4ef8cf0ad604af02d2b72e
Sonicdaheghod <68253811+Sonicdaheghod@users.noreply.github.com>
2023-03-27 16:56:34 -0400

    Update Question9.Date.py

--- Question9.Date.py
+++ Question9.Date.py
@@ -2,6 +2,14 @@
 #exam_st_date = (11, 12, 2014)
 #Sample Output : The examination will start from : 11 / 12 / 2014
 
-from datetime import date
-examDate = (2/15/2023)
-examText = ("The examination will start from: ", date(examDate)
+
+#my attempt
+examDate = (11,12,2014)
+print("The exam starts on:",examDate[0],"/",examDate[1],"/",examDate[2])
+
+#answer
+exam_st_date = (11,12,2014)
+print( "The examination will start from : %i / %i / %i"%exam_st_date)
+
+#%i = integer
+#we extract the integers and put them in form "x/x/x" from our exam date variable
