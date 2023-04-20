22eae088a123d507be0dd07d98bd7945f49fa54e
Sonicdaheghod <68253811+Sonicdaheghod@users.noreply.github.com>
2023-04-19 20:40:19 -0400

    Add files via upload

---
+++ Question20.printString.py
@@ -0,0 +1,7 @@
+# 20. Write a Python program that returns a string that is n (non-negative integer) copies of a given string.
+userInput = input("How many times do you want your string to be written? ")
+stringPreference = input("Write something that's on your mind: ")
+x = range(int(userInput))
+
+for n in x:
+  print(stringPreference)
