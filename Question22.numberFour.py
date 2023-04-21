74ded6a49c02d0727cb62a06f2ae9e5da4e399b7
Megan Tran <68253811+Sonicdaheghod@users.noreply.github.com>
2023-04-21 05:52:21 -0400

    Add files via upload

---
+++ Question22.numberFour.py
@@ -0,0 +1,14 @@
+# 22. Write a Python program to count the number 4 in a given list.
+
+#my attempt
+# list = [1,2,3,4,24]
+
+# if list == 4:
+#     print("There is a 4")
+
+#correct answer
+list = [1,2,3,4,24]
+
+numberCount = list.count(4)
+
+print("The number of 4's in the list is",numberCount)
